f478f06814bd0af04771687c5db2720a23c35ca5
wraithxlaksh <wraithxlaksh@gmail.com>
2026-03-09 20:51:50 +0530

    update4

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -103,7 +103,7 @@ const Timeline = () => {
     {
       month: 'March 2026',
       events: [
-        { date: '10', title: 'GTA IV Physics Workshop' },
+        { date: '10', title: 'Game Theory Workshop' },
       ]
     },
     {
