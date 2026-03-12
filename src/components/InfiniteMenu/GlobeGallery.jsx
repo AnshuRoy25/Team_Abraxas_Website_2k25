70e40565eb4781e32cd7efcf6ebe0461bcdc67d6
AnshuRoy25 <24bph025@nith.ac.in>
2026-03-13 01:09:23 +0530

    fixed explore more

--- src/components/InfiniteMenu/GlobeGallery.jsx
+++ src/components/InfiniteMenu/GlobeGallery.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import InfiniteMenu from './InfiniteMenu';
+import { Link } from 'react-router-dom';
 
 const items = [
   // 2025
@@ -85,16 +86,16 @@ const GlobeGallery = () => {
           <InfiniteMenu items={items} scale={1} />
         </div>
 
-        <a
-          href="/Gallery"
-          style={{ fontFamily: "'DM Sans', sans-serif" }}
-          className="absolute bottom-4 right-4 z-20 inline-flex items-center gap-2 px-4 py-2
-            bg-black/70 border border-white/25 rounded-xl text-white/70 text-xs font-medium
-            hover:bg-white/10 hover:text-white hover:border-white/50
-            backdrop-blur-sm transition-all duration-300"
-        >
-          Explore More
-        </a>
+        <Link
+  to="/Gallery"
+  style={{ fontFamily: "'DM Sans', sans-serif" }}
+  className="absolute bottom-4 right-4 z-20 inline-flex items-center gap-2 px-4 py-2
+    bg-black/70 border border-white/25 rounded-xl text-white/70 text-xs font-medium
+    hover:bg-white/10 hover:text-white hover:border-white/50
+    backdrop-blur-sm transition-all duration-300"
+>
+  Explore More
+</Link>
       </motion.div>
     </div>
   );
